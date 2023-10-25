104213653cd37966be8890e25010764c844a5b10
guimochi <yamamoto.guilherme@outlook.com>
2023-10-25 12:13:02 +0200

    Added base for stocks and users

--- orders/src/main/java/vinci/stock/orders/OrdersApplication.java
+++ orders/src/main/java/vinci/stock/orders/OrdersApplication.java
@@ -1,4 +1,4 @@
-package vinci.stock.users;
+package vinci.stock.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,12 +6,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients
-public class UsersApplication {
+@EnableDiscoveryClient
+public class OrdersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UsersApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(OrdersApplication.class, args);
+  }
 
 }
---
+++ stocks/src/test/java/vinci/stock/orders/StocksApplicationTests.java
@@ -0,0 +1,13 @@
+package vinci.stock.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
