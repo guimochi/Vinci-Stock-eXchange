1e33daad2b65b5f625f5de8992eb4a5f6b077fe0
guimochi <yamamoto.guilherme@outlook.com>
2023-10-25 12:43:19 +0200

    Added base for prices

---
+++ prices/src/test/java/vinci/stock/prices/PricesApplicationTests.java
@@ -0,0 +1,13 @@
+package vinci.stock.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
