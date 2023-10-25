919f83e50fa8b5a1697db48d98c01a28bf291105
guimochi <yamamoto.guilherme@outlook.com>
2023-10-25 12:35:39 +0200

    Added base for authentication

---
+++ authentication/src/main/java/vinci/stock/authentication/AuthenticationApplication.java
@@ -0,0 +1,16 @@
+package vinci.stock.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationApplication.class, args);
+	}
+
+}
